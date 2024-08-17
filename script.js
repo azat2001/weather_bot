75e9382107aac7479173ed4bef0f5af09e2a2119
azat20014 <azat.garifullin.2001@yandex.ru>
2024-08-17 16:17:18 +0500

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-bot.command('start', (ctx) => {
-    ctx.reply('Откройте погодный информер', {
-        reply_markup: {
-            inline_keyboard: [[{text: "Погода", web_app: {url: "https://ваш_адрес.github.io"}}]]
-        }
-    });
-});
